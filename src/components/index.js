fcc5abb7d8c1c965afd3de50c79f6cf1593242de
Sarthak Thakur <50295928+SarthakThakur0097@users.noreply.github.com>
2020-10-01 14:20:55 -0400

    Added Particle effect

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,63 @@
 import React from 'react'
 import NavBar from "./Navbar"
+import Header from "./Header"
+import Particles from "react-particles-js"
+import {makeStyles, mergeClasses} from "@material-ui/styles"
+
+const useStyles = makeStyles({
+    particlesCanva: {
+        position: "absolute"
+    }
+})
 const Home = () => {
+    const classes = useStyles()
     return (
-        <div>
+        <>
+        
            <NavBar/>
-        </div>
+           <Header/>
+           <Particles
+           canvasClassName={classes.particlesCanva}
+            param={{
+            particles:{
+                number:{
+                    value:45,
+                    density: {
+                        enable: true,
+                        value_area:900
+                    }
+                },
+                shape: {
+                    type: "circle",
+                    stroke: {
+                        width: 1,
+                        color: 'tomato'
+                    }
+                },
+                size: {
+                    value: 8, 
+                    random: true,
+                    anim: {
+                        enable: false,
+                        speed:8,
+                        size_min: 0.1,
+                        sync: true
+                    }
+                },
+                opacity: {
+                    value: 1,
+                    random: true,
+                    anim: {
+                        enable: true,
+                        speed: 1,
+                        opacity_min: 0.1,
+                        sync: true
+                    }
+                }
+            }
+        }}
+        />
+        </>
     )
 }
 
